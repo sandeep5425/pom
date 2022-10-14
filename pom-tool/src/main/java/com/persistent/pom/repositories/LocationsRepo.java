765ba8ccf1c3386b570bd1297f389fbefb55fdd5
Sandeep Madhiri <sandeep_madhiri@persistent.com>
2022-10-14 11:43:30 +0530

    Removed conflicts

--- pom-tool/src/main/java/com/persistent/pom/repositories/LocationsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.persistent.pom.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.persistent.pom.entities.Location;
-
-public interface LocationsRepo extends CrudRepository<Location, Integer> {
-	
-}
